64d9476302a1da40e66768286749a4386250908c
Byeongsoon Jang <byeongsoon94@gmail.com>
2021-10-11 20:42:16 +0900

    modify Pos

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,41 +2,17 @@ fun main() {
 
   val myPos = Pos("byeongsoon")
   myPos.start()
-  myPos.viewItemList()
+  myPos.printAll()
 
   myPos.process()
 
-  myPos.addItem("감자칩",2000)
-  myPos.viewItemList()
+  myPos.create("감자칩",2000)
+  myPos.printAll()
 
-  myPos.removeItem(2)
-  myPos.viewItemList()
+  myPos.remove(2)
+  myPos.printAll()
 
   myPos.exit()
-
-  // todo : while loop 돌면서 입력받아서 기능 수행
-  while (!true) {
-    var input: Int = 1 // 입력 받아야함
-    if (input in 1..5) {
-      println("올바른 명령어가 아닙니다.")
-      return
-    }
-
-    when(input) {
-      1 -> {
-        myPos.viewItemList()
-      }
-      2 -> {
-        // todo : 새로 등록할 물품 입력받기
-        val newItemName = "바나나우유"
-        val newItemPrice = 1200
-        myPos.addItem(newItemName,newItemPrice)
-      }
-    }
-
-    // todo : when 이용하여 입력값별로 프로세스 수행
-
-  }
 
 }
 
--- src/main/kotlin/Pos.kt
+++ src/main/kotlin/Pos.kt
@@ -7,7 +7,7 @@ class Pos(
   fun start() {
     println("***********Kotlin 편의점***********")
     println("POS 실행중....")
-    makeItemList()
+    init()
     println("접속 직원은 ${staff}님 입니다.")
     println("***********환영합니다***********")
     println()
@@ -22,9 +22,33 @@ class Pos(
     println("*****************작업 선택*****************")
     println("1. 상품조회 | 2. 상품등록 | 3. 상품삭제 | 4.결재")
     println()
+
+    // todo : while loop 돌면서 입력받아서 기능 수행
+    while (!true) {
+      var input: Int = 1 // 입력 받아야함
+      if (input in 1..5) {
+        println("올바른 명령어가 아닙니다.")
+        return
+      }
+
+      when(input) {
+        1 -> {
+          printAll()
+        }
+        2 -> {
+          // todo : 새로 등록할 물품 입력받기
+          val newItemName = "바나나우유"
+          val newItemPrice = 1200
+          create(newItemName,newItemPrice)
+        }
+      }
+
+      // todo : when 이용하여 입력값별로 프로세스 수행
+
+    }
   }
 
-  fun viewItemList() {
+  fun printAll() {
     println("***********현재 판매중인 상품은***********")
     var i = 0
       for (item in itemList) {
@@ -34,7 +58,7 @@ class Pos(
     println()
   }
 
-  fun addItem(name: String, price:Int) {
+  fun create(name: String, price:Int) {
     println("상품 등록중...")
     val newItem = Item(name, price)
     itemList.add(newItem)
@@ -43,18 +67,28 @@ class Pos(
     println()
   }
 
-  fun removeItem(index: Int) {
+  fun remove(index: Int) {
     println("상품 삭제중..")
     itemList.removeAt(index - 1)
     println("상품 삭제가 완료되었습니다.")
     println()
   }
 
-  fun makeItemList() {
+  fun init() {
     println("inti()...")
 
     val items = listOf<Item>(Item("치토스",1000),Item("참이슬",1500),Item("파워에이드",2000),Item("신라면",950))
     itemList.addAll(items)
+  }
+
+  fun payment(paymentList: List<Int>): Int {
+    var totalPrice = 0
+    for (payment in paymentList) {
+      totalPrice += itemList.get(payment).price
+//      totalPrice += itemList[payment].price
+    }
+
+    return totalPrice
   }
 
 }
